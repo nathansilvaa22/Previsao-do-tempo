59220a0d23cf84a4b36c7a2e68cee68c66486faa
Nathan Silva <claricelucasmoreira8@gmail.com>
2024-06-12 18:14:23 -0300

    add as pastas do projeto

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+const key = "068cfa227838220445ad7b5ce8c80d2d"
+
+  function colocarDadosNaTela(dados){
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+
+    document.querySelector(".temp").innerHTML =Math.floor( dados.main.temp)+ " °C "
+
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+ }
+
+ async function buscarCidade(cidadee){
+
+     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidadee}&appid=${ key}&lang=pt_br&units=metric`). then(resposta => resposta.json())
+
+     colocarDadosNaTela(dados)
+    
+}
+
+
+
+
+function cliqueiNoBotao(){
+    const cidadee = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidadee)
+}
